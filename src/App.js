4fc29351bc33a5d8803a06fb5c723a16cb8a4639
Shiva Reddy <shivareddy@Shivas-MacBook-Air.local>
2021-07-11 11:43:26 +0530

    added placeholder

--- src/App.js
+++ src/App.js
@@ -26,7 +26,7 @@ console.log("How is your health Mr. Shiva Reddy?");
       </Route>
       </Switch>
     </div>
-<input type="text"/>
+<input type="text" placeholder="email"/>
     </Router>
 
   );
