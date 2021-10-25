9af2fe8d751e9fab65613ca7789a2de88028b411
IlyasHussain07 <hilyas904@gmail.com>
2021-10-25 12:31:25 +0530

    adding footer

--- src/App.js
+++ src/App.js
@@ -3,30 +3,32 @@ import Header from './Header';
 import './App.css';
 import Home from './Home';
 import Checkout from "./Checkout";
-import {BrowserRouter as Router, Switch, Route} from "react-router-dom";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import SignIn from "./SignIn";
+import Footer from './footer'
 
 function App() {
-console.log("How are you Mr. Shiva Reddy?");
-console.log("How is your health Mr. Shiva Reddy?");
-//can you slove the issue by committing the changes in firebase app
+  console.log("How are you Mr. Shiva Reddy?");
+  console.log("How is your health Mr. Shiva Reddy?");
+  //can you slove the issue by committing the changes in firebase app
   return (
     <Router>
-    <div className="app">
-    <Switch>
-    <Route path="/Sign in">
-      <SignIn/>
-      </Route>
-      <Header/>
-        <Route path="/Checkout">
-      <Checkout/>
-      </Route>
-      <Route path="/">
-      <Home/>
-      </Route>
-      </Switch>
-    </div>
-<input type="text" placeholder="email"/>
+      <div className="app">
+        <Switch>
+          <Route path="/Sign in">
+            <SignIn />
+          </Route>
+          <Header />
+          <Route path="/Checkout">
+            <Checkout />
+          </Route>
+          <Route path="/">
+            <Home />
+            <Footer />
+          </Route>
+        </Switch>
+      </div>
+      <input type="text" placeholder="email" />
     </Router>
 
   );
